223eaace5de7ab69c6a63235ca1951874ac5686f
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-03-13 09:12:02 +0100

    REST impl

--- src/main/java/pl/brzezinski/CarShop/dao/DriverRepositoryDataJpaImpl.java
+++ src/main/java/pl/brzezinski/CarShop/dao/DriverRepositoryDataJpaImpl.java
@@ -5,4 +5,7 @@ import pl.brzezinski.CarShop.model.Driver;
 
 public interface DriverRepositoryDataJpaImpl extends JpaRepository<Driver, Long> {
 
+    Driver findDriverByLastName(String lastName);
+
+    Driver findDriverByFirstName(String firstName);
 }
